61b36b3c8e6aa33ce38ac45f7986c648fa1f120c
yassinezayane11 <yassinezayane11@gmail.com>
2026-05-11 20:16:44 +0100

    v2

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,6 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // ✅ ROOT (باش ماعادش يطلع Cannot GET /)
 app.get('/', (req, res) => {
